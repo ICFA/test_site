bef673440f2a034485853de0c8adfed19c83f8c0
ICFA <87987489+ICFA@users.noreply.github.com>
2024-05-01 21:28:46 +0500

    Add files via upload

--- site_app/static/js/script.js
+++ site_app/static/js/script.js
@@ -9,3 +9,22 @@ function change_text_size(){
 function change_position(){
     document.body.classList.toggle('text-align-center')
 }
+
+function calculation(){
+    var list = document.querySelectorAll('#tala');
+    var v = [];
+    for (let i = 0; i < list.length; i++) {
+        v.push(parseFloat(list[i].textContent.split(": ")[1].replace("%", "")));
+      }
+    var avg = v.reduce( ( p, c ) => p + c, 0 ) / v.length;
+    var result = [v.min(), avg.toFixed(1), v.max()];
+    document.getElementById("demonstrate").innerHTML = result.join(" / ");
+}
+
+Array.prototype.min = function() {
+    return Math.min.apply(null, this);
+};
+
+Array.prototype.max = function() {
+    return Math.max.apply(null, this);
+};
